e8a9c57ff0b4d543765060fd00b01c9b83dd5166
NameSasipha <sasipha_th@cmu.ac.th>
2023-08-12 18:22:12 +0700

    add type.ts

---
+++ src/type.ts
@@ -0,0 +1,26 @@
+export interface StudentDetail {
+    id: number
+    FirstName: number
+    LastName: string
+    Student_ID: string
+    registered: string
+    advisor: number
+    stu_pic: string
+        
+}
+
+export interface TeacherDetail {
+    id: number
+    FirstName: string
+    LastName: string
+    Department: string
+    Email: string
+    Office: string
+    advisor_pic: string
+
+}
+
+
+
+
+
