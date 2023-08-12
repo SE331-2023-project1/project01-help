bf6267e6fb09a56d61f255fd2b54f3f8571e2d7c
ptpofficialxd <aekkungzaza@gmail.com>
2023-08-12 15:25:40 +0700

    import component student/teacher view to index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/StudentView.vue'
+import StudentView from '../views/StudentView.vue'
+import TeacherView from '../views/TeacherView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'student',
-      component: HomeView
+      component: StudentView
     },
     {
       path: '/teacher',
@@ -15,7 +16,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/TeacherView.vue')
+      component: TeacherView
     }
   ]
 })
