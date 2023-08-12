60dd1f1585e26a9cba235103262fd2f606ac2030
ptpofficialxd <aekkungzaza@gmail.com>
2023-08-12 16:24:39 +0700

    add tailwind style to Teacher Div

--- src/components/TeacherCard.vue
+++ src/components/TeacherCard.vue
@@ -1,10 +1,8 @@
 <template>
-    <div class="teacherDetail-class">
-      <div class="teacherDetail-class">
-        <span> First Name: {{ teacherDetail.name }}</span>
-        <p>Last Name: {{ teacherDetail.surName }}</p>
-        <p>Advisor ID: {{ teacherDetail.id }}</p>
-      </div>
+    <div class="border-2 border-white-w20-h20 bg-blue-500">
+      <span> First Name: {{ teacherDetail.name }}</span>
+      <p>Last Name: {{ teacherDetail.surName }}</p>
+      <p>Advisor ID: {{ teacherDetail.id }}</p>
     </div>
   </template>
   
@@ -14,14 +12,13 @@
   </script>
   
   <style scoped>
-  .teacherDetail-class {
-    padding: 20px;
+  /* .teacherDetail-class {
     width: 250px;
     cursor: pointer;
     border: 1 px solid #39495c;
     margin-top: 20px;
     margin-bottom: 18px;
-  }
+  } */
   
   .teacherDetail-class:hover {
     transform: scale(1.01);
