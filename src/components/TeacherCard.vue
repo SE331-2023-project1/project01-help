f77b7ef145b59c369d60d47af02e504218c6a7cb
NameSasipha <sasipha_th@cmu.ac.th>
2023-08-13 13:51:45 +0700

    show TeacherCard

--- src/components/TeacherCard.vue
+++ src/components/TeacherCard.vue
@@ -1,28 +1,41 @@
 <template>
-    <div class="border-2 border-white-w20-h20 bg-blue-500">
-      <span> First Name: {{ teacherDetail.name }}</span>
-      <p>Last Name: {{ teacherDetail.surName }}</p>
-      <p>Advisor ID: {{ teacherDetail.id }}</p>
-    </div>
-  </template>
   
-  <script setup lang="ts">
-  import { ref } from 'vue'
-  const teacherDetail = ref({})
-  </script>
+  <div v-if="advisor" class="StudentDetail-class">
+    <span> First Name: {{ advisor.FirstName }}</span>
+    <p>Last Name: {{ advisor.LastName }}</p>
+    <p>Department: {{ advisor.Department}}</p>
+    <p>Email: {{ advisor.Email }}</p>
+    <p>Office: {{ advisor.Office}}</p>
+    <img :src = "advisor.advisor_pic" alt="img">
   
-  <style scoped>
-  /* .teacherDetail-class {
-    width: 250px;
-    cursor: pointer;
-    border: 1 px solid #39495c;
-    margin-top: 20px;
-    margin-bottom: 18px;
-  } */
-  
-  .teacherDetail-class:hover {
-    transform: scale(1.01);
-    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-  }
-  </style>
-  
+</div>
+</template>
+
+<script setup lang="ts">
+import { ref, type PropType } from 'vue'
+import {type TeacherDetail } from '@/type';
+
+const TeacherDetail= ref({})
+const props = defineProps({
+  advisor:{
+  type: Object as PropType<TeacherDetail>,
+  required:true
+}
+})
+</script>
+
+<style scoped>
+.advisorDetail-class {
+padding: 20px;
+width: 250px;
+cursor: pointer;
+border: 1 px solid #39495c;
+margin-top: 20px;
+margin-bottom: 18px;
+}
+
+.advisorDetail-class:hover {
+transform: scale(1.01);
+box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
