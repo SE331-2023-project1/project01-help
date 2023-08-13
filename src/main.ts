ffa80e71945f15defacce97155bee8a6bc184c8e
NameSasipha <sasipha_th@cmu.ac.th>
2023-08-13 11:20:30 +0700

    Create NProgress bar

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import 'nprogress/nprogress.css'
 import App from './App.vue'
 import router from './router'
 
