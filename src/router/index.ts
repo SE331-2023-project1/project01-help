7698aa0f14b637af80a85dd793656383cf9cb3ad
NameSasipha <sasipha_th@cmu.ac.th>
2023-08-24 15:17:56 +0700

    try to add some api

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,8 @@ import StudentView from '../views/StudentView.vue'
 import TeacherView from '../views/TeacherView.vue'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import StudentService from '@/services/StudentService'
+import type { StudentDetail, TeacherDetail } from '@/type'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,5 +31,40 @@ router.beforeEach(() => {
 router.afterEach(() => {
   NProgress.done()
 })
+
+
+// const fetchStudents = async () => {
+//   try {
+//     const response = await StudentService.getStudent();
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching students:', error);
+//     return [];
+//   }
+// };
+
+// router.beforeEach(async () => {
+//   const studentData: StudentDetail[] = await fetchStudents();
+//   StudentService.getStudent(studentData);
+// })
+
+// const fetchTeacher = async () => {
+//   try {
+//     const response = await TeacherService.getTeacher();
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching Teacher:', error);
+//     return [];
+//   }
+// };
+
+// router.beforeEach(async () => {
+//   const TeacherData: TeacherDetail[] = await fetchTeacher();
+//   StudentService.getStudent(TeacherData);
+// })
+
+
+
+
 
 export default router
