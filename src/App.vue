43bad54dc14eb342f2a07f8f46576c1f2af6d7a8
Baibua <siriyakorn14869@gmail.com>
2023-08-24 17:43:55 +0700

    fix tailwind

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,75 @@ import { RouterLink, RouterView } from "vue-router"
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/management.png" width="125" height="125" />
+
+    <nav>
+      <ul class="flex border-b">
+  <li class="-mb-px mr-1">
+    <div class="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" >
+      <RouterLink to="/">Student</RouterLink>
+    </div>
+  </li>
+
+  <li class="mr-1">
+    <div class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold">
+      <RouterLink to="/advisor">Advisor</RouterLink>
+    </div>
+  </li>
+
+  <li class="ml-auto mt-3 " >
+    <div class="">
+      <button class="w-4 h-4"><img src="./assets/add.png" alt="addButton">
+      <RouterLink to="/addPerson"></RouterLink>
+      </button>
+        
+    </div>
+  </li>
+  
+</ul>
+    </nav>
+
+
+
+    <!-- <div> เป็นไรไม่รู้ เข้า studentDetail แล้วขึ้น 2อัน
+    <nav>
+      <ul class="flex border-b">
+        <li class="-mb-px mr-1">
+          <div class="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold">
+            <router-link to="/">Student</router-link>
+          </div>
+        </li>
+
+        <li class="mr-1">
+          <div class="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold">
+            <router-link to="/advisor">Advisor</router-link>
+          </div>
+        </li>
+
+      </ul>
+    </nav>
+
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div> -->
+
+
+
+
+
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/management.png" width="125" height="125" />
     <div class="wrapper">
       <nav class="font-mono text-yellow-300" >
         <RouterLink to="/">Student</RouterLink>
         <RouterLink to="/advisor">Advisor</RouterLink>
       </nav>
-    </div>
+    </div> -->
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
+<!-- <style scoped>
   header {
   line-height: 1.5;
   max-height: 100vh;
@@ -78,5 +134,14 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
+}
+</style> -->
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
